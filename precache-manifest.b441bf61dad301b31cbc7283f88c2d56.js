e4aee7357bac320f56d328668abf70c33060ce26
Tushar Ranka <tushar@theragun.com>
2019-12-16 10:31:20 -0800

    Updates

---
+++ precache-manifest.b441bf61dad301b31cbc7283f88c2d56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506e994bd8ff7bd2fea20c3c402e96e7",
+    "url": "/nu_gh/index.html"
+  },
+  {
+    "revision": "5c8465334ad348c474d9",
+    "url": "/nu_gh/static/css/main.0ba23238.chunk.css"
+  },
+  {
+    "revision": "d23e9f754e75ba1fe281",
+    "url": "/nu_gh/static/js/2.1abde296.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nu_gh/static/js/2.1abde296.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c8465334ad348c474d9",
+    "url": "/nu_gh/static/js/main.8fa0d9a4.chunk.js"
+  },
+  {
+    "revision": "0bab044fe5f32e937deb",
+    "url": "/nu_gh/static/js/runtime-main.599de68b.js"
+  }
+]);
